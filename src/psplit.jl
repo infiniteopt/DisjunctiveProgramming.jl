fff06f274fd41ecf4a6b3f556b00dbdeecf31155
d227nguyen <d227nguy@uwaterloo.ca>
2025-07-04 14:09:54 -0400

    Initial nonlinear dispatch for _build_partitioned_expression

--- src/psplit.jl
+++ src/psplit.jl
@@ -1,5 +1,9 @@
 #Extending reformulate_disjunction in order to get all possible variables in the disjuncts
-
+#TODO: Dispatch over Nonlinear expressions
+#TODO: Create function to handle bounds of the auxiliary variables (involves solving max/min problem with just variable constraints)
+#TODO: Verify we can handle multiple constraints per disjunct (It can for quadratic/linears)
+#TODO: Detect nonseperable constraints and throw error
+#TODO: Test nonlinear stuff
 function reformulate_disjunct_constraint(
     model::JuMP.AbstractModel,
     con::JuMP.ScalarConstraint{T, S},
@@ -11,15 +15,15 @@ function reformulate_disjunct_constraint(
     v = [@variable(model) for _ in 1:length(method.partition)]
     
     for i in 1:length(method.partition)
-        reform_con[i] = JuMP.build_constraint(error, build_partitioned_expression(con.func, method.partition[i]) - v[i], MOI.LessThan(0.0))
+        reform_con[i] = JuMP.build_constraint(error, _build_partitioned_expression(con.func, method.partition[i]) - v[i], MOI.LessThan(0.0))
     end
     reform_con[end] = JuMP.build_constraint(error, sum(v[i] * bvref for i in 1:length(v)) - con.set.upper * bvref, MOI.LessThan(0.0))
     return reform_con
 end
 
-function build_partitioned_expression(
+function _build_partitioned_expression(
     expr::JuMP.GenericAffExpr,
-    partition_variables::Vector{JuMP.VariableRef},
+    partition_variables::Vector{JuMP.VariableRef}
 )
     new_affexpr = AffExpr(0.0, Dict{JuMP.VariableRef,Float64}())
     for var in partition_variables
@@ -29,9 +33,9 @@ function build_partitioned_expression(
     return new_affexpr
 end
 
-function build_partitioned_expression(
+function _build_partitioned_expression(
     expr::JuMP.GenericQuadExpr,
-    partition_variables::Vector{JuMP.VariableRef},
+    partition_variables::Vector{JuMP.VariableRef}
 )
     
     new_quadexpr = QuadExpr(0.0, Dict{JuMP.VariableRef,Float64}())
@@ -43,15 +47,66 @@ function build_partitioned_expression(
     return new_quadexpr
 end
 
-# function bound_auxiliary_variables(
-#     expr::JuMP.GenericAffExpr, 
-#     aux_var::JuMP.VariableRef,
-#     method::PSplit)
-    
-#     bound_problem = Model(Gurobi.Optimizer)
+function _build_partitioned_expression(
+    expr::JuMP.VariableRef,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    if expr in partition_variables
+        return expr
+    else
+        return 0
+    end
+end
 
+function _build_partitioned_expression(
+    expr::Number,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    return expr
+end
 
-
-    
+# function replace_variables_in_constraint(fun::NonlinearExpr, var_map::Dict{VariableRef, VariableRef})
+#     new_args = Any[replace_variables_in_constraint(arg, var_map) for arg in fun.args]
+#     return JuMP.NonlinearExpr(fun.head, new_args)
 # end
-    
+
+
+function contains_only_partition_variables(
+    expr::JuMP.VariableRef,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    return expr in partition_variables
+end
+
+function contains_only_partition_variables(
+    expr::Number,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    return true
+end
+
+#Helper functions for the nonlinear case.
+function contains_only_partition_variables(
+    expr::JuMP.NonlinearExpr,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    return all(contains_only_partition_variables(arg, partition_variables) for arg in expr.args)
+end
+
+function _build_partitioned_expression(
+    expr::JuMP.NonlinearExpr,
+    partition_variables::Vector{JuMP.VariableRef}
+)
+    if expr.head == :+
+        return JuMP.NonlinearExpr(
+            :+, 
+            (_build_partitioned_expression(arg, partition_variables) for arg in expr.args)...
+        )
+    end
+
+    if contains_only_partition_variables(expr, partition_variables)
+        return expr
+    else
+        return 0
+    end
+end
