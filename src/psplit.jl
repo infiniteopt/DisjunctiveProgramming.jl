2c286e1c3fc25b5bc1be995625d7111a4b7197b5
dnguyen227 <d227nguy@uwaterloo.ca>
2025-08-22 11:31:57 -0300

    Vector excluding NL works.

--- src/psplit.jl
+++ src/psplit.jl
@@ -1,35 +1,35 @@
 #TODO: If the constraint is Nonlinear -> Throw a warning
+#TODO: Fix vectors in general
+
 function _build_partitioned_expression(
     expr::JuMP.GenericAffExpr,
-    partition_variables::Vector{JuMP.VariableRef},
-    ::Any
+    partition_variables::Vector{JuMP.VariableRef}
 )
+    constant = JuMP.constant(expr)
     new_affexpr = AffExpr(0.0, Dict{JuMP.VariableRef,Float64}())
     for var in partition_variables
         add_to_expression!(new_affexpr, coefficient(expr, var), var) 
     end
-
-    return new_affexpr, 0
+    return new_affexpr, constant
 end
 
 function _build_partitioned_expression(
     expr::JuMP.GenericQuadExpr,
-    partition_variables::Vector{JuMP.VariableRef},
-    ::Any
+    partition_variables::Vector{JuMP.VariableRef}
 )
     new_quadexpr = QuadExpr(0.0, Dict{JuMP.VariableRef,Float64}())
+    constant = JuMP.constant(expr)
     for var in partition_variables
         add_to_expression!(new_quadexpr, get(expr.terms, JuMP.UnorderedPair(var, var), 0.0), var,var) 
         add_to_expression!(new_quadexpr, coefficient(expr, var), var) 
     end
 
-    return new_quadexpr, 0
+    return new_quadexpr, constant
 end
 
 function _build_partitioned_expression(
     expr::JuMP.VariableRef,
-    partition_variables::Vector{JuMP.VariableRef},
-    ::Any
+    partition_variables::Vector{JuMP.VariableRef}
 )
     if expr in partition_variables
         return expr, 0
@@ -40,8 +40,7 @@ end
 
 function _build_partitioned_expression(
     expr::Number,
-    partition_variables::Vector{JuMP.VariableRef},
-    ::Any
+    partition_variables::Vector{JuMP.VariableRef}
 )
     return expr, 0
 end
@@ -49,20 +48,19 @@ end
 
 function _build_partitioned_expression(
     expr::JuMP.NonlinearExpr,
-    partition_variables::Vector{JuMP.VariableRef},
-    con::JuMP.ScalarConstraint
+    partition_variables::Vector{JuMP.VariableRef}
 )
     if expr.head in (:+, :-)
-        rhs = get(filter(x -> isa(x, Number), expr.args), 1, 0.0)
+        constant = get(filter(x -> isa(x, Number), expr.args), 1, 0.0)
         if expr.head == :+
-            rhs = -rhs
+            constant = -constant
         end
-        new_func = _nonlinear_recursion(expr, partition_variables, con) + rhs
+        new_func = _nonlinear_recursion(expr, partition_variables) + constant
     else
-        new_func = _nonlinear_recursion(expr, partition_variables, con)
-        rhs = 0.0
+        new_func = _nonlinear_recursion(expr, partition_variables)
+        constant = 0.0
     end
-    return new_func, rhs
+    return new_func, constant
 end
 
 function contains_only_partition_variables(
@@ -98,28 +96,23 @@ function contains_only_partition_variables(
     return all(contains_only_partition_variables(arg, partition_variables) for arg in expr.args)
 end
 
-
-
-
 function _nonlinear_recursion(
     expr::Union{JuMP.GenericAffExpr, JuMP.VariableRef, JuMP.GenericQuadExpr, Number},
-    partition_variables::Vector{JuMP.VariableRef},
-    con::JuMP.ScalarConstraint
+    partition_variables::Vector{JuMP.VariableRef}
 )
-    new_func, _ = _build_partitioned_expression(expr, partition_variables, con)
+    new_func, _ = _build_partitioned_expression(expr, partition_variables)
     return new_func
 end
 
 
 function _nonlinear_recursion(
     expr::JuMP.NonlinearExpr,
-    partition_variables::Vector{JuMP.VariableRef},
-    con::JuMP.ScalarConstraint,
+    partition_variables::Vector{JuMP.VariableRef}
     )
     if expr.head in (:+, :-)
         return JuMP.NonlinearExpr(
             expr.head,
-            (_nonlinear_recursion(arg, partition_variables, con) for arg in expr.args)...
+            (_nonlinear_recursion(arg, partition_variables) for arg in expr.args)...
         )
     end
     if contains_only_partition_variables(expr, partition_variables)
@@ -185,8 +178,8 @@ function set_variable_bound_info(vref::JuMP.AbstractVariableRef, ::PSplit)
     if !has_lower_bound(vref) || !has_upper_bound(vref)
         error("Variable $vref must have both lower and upper bounds defined when using the PSplit reformulation.")
     else
-        lb = min(0, lower_bound(vref))
-        ub = max(0, upper_bound(vref))
+        lb = lower_bound(vref)
+        ub = upper_bound(vref)
     end
     return lb, ub
 end
@@ -200,10 +193,10 @@ function reformulate_disjunct_constraint(
 ) where {T, S <: _MOI.LessThan}
     p = length(method.partition)
     v = [@variable(model) for _ in 1:p]
-    _, rhs = _build_partitioned_expression(con.func, method.partition[p], con)
+    _, rhs = _build_partitioned_expression(con.func, method.partition[p])
     reform_con = Vector{JuMP.AbstractConstraint}(undef, p + 1)
     for i in 1:p
-        func, _ = _build_partitioned_expression(con.func, method.partition[i], con)
+        func, _ = _build_partitioned_expression(con.func, method.partition[i])
         reform_con[i] = JuMP.build_constraint(error, func - v[i], MOI.LessThan(0.0))
         _bound_auxiliary(model, v[i], func, method)
     end
@@ -220,10 +213,10 @@ function reformulate_disjunct_constraint(
     p = length(method.partition)
     reform_con = Vector{JuMP.AbstractConstraint}(undef, p + 1)
     v = [@variable(model) for _ in 1:p]
-    _, rhs = _build_partitioned_expression(con.func, method.partition[p], con)
+    _, rhs = _build_partitioned_expression(con.func, method.partition[p])
     
     for i in 1:p
-        func, _ = _build_partitioned_expression(con.func, method.partition[i], con)
+        func, _ = _build_partitioned_expression(con.func, method.partition[i])
         reform_con[i] = JuMP.build_constraint(error, -func + v[i], MOI.LessThan(0.0))
         _bound_auxiliary(model, v[i], -func, method)
     end
@@ -242,10 +235,10 @@ function reformulate_disjunct_constraint(
     reform_con_lt = Vector{JuMP.AbstractConstraint}(undef, p + 1)
     reform_con_gt = Vector{JuMP.AbstractConstraint}(undef, p + 1)
     #let [_, 1] be the upper bound and [_, 2] be the lower bound
-    _, rhs = _build_partitioned_expression(con.func, method.partition[p], con) 
+    _, rhs = _build_partitioned_expression(con.func, method.partition[p]) 
     v = @variable(model, [1:p, 1:2])
     for i in 1:p
-        func, _= _build_partitioned_expression(con.func, method.partition[i], con)
+        func, _= _build_partitioned_expression(con.func, method.partition[i])
         reform_con_lt[i] = JuMP.build_constraint(error, func - v[i,1], MOI.LessThan(0.0))
         reform_con_gt[i] = JuMP.build_constraint(error, -func + v[i,2], MOI.LessThan(0.0))
         _bound_auxiliary(model, v[i,1], func, method)
@@ -257,29 +250,98 @@ function reformulate_disjunct_constraint(
     #TODO: how do i avoid the vcat?
     return vcat(reform_con_lt, reform_con_gt)
 end
+#Functions
 function reformulate_disjunct_constraint(
     model::JuMP.AbstractModel,
     con::JuMP.VectorConstraint{T, S, R},
     bvref::Union{JuMP.AbstractVariableRef, JuMP.GenericAffExpr},
     method::PSplit
-) where {T, S <: Union{_MOI.Nonpositives,_MOI.Nonnegatives, _MOI.Zeros}, R}
+) where {T, S <: Union{_MOI.Nonpositives}, R}
     p = length(method.partition)
     d = con.set.dimension
     v = @variable(model, [1:p,1:d])
     reform_con = Vector{JuMP.AbstractConstraint}(undef, p + 1)
+    constants = Vector{Number}(undef, d)
     for i in 1:p
-        func, _ = JuMP.@expression(model, [j = 1:d], _build_partitioned_expression(con.func[j], method.partition[i], con) - v[i,j])
-        reform_con[i] = JuMP.build_constraint(error, func, con.set)
+        #I should be subtracting the constant here.
+        partitioned_expressions = [_build_partitioned_expression(con.func[j], method.partition[i]) for j in 1:d]
+        func = JuMP.@expression(model, [j = 1:d], partitioned_expressions[j][1] - v[i,j])
+        constants .= [-partitioned_expressions[j][2] for j in 1:d]
+        reform_con[i] = JuMP.build_constraint(error, func, _MOI.Nonpositives(d))
         for j in 1:d
             _bound_auxiliary(model, v[i,j], func[j], method)
         end
+        println("Reformulated constraint $i: ", reform_con[i])
+        println("Constants: ", constants)
     end
-    new_func = JuMP.@expression(model,[j = 1:d], sum(v[i,j] * bvref for i in 1:p) + JuMP.constant(con.func[j])*bvref)
-    reform_con[end] = JuMP.build_constraint(error, new_func, con.set)
-    #TODO: how do i avoid the vcat?
+    new_func = JuMP.@expression(model,[j = 1:d], sum(v[i,j] * bvref for i in 1:p) - constants[j]*bvref)
+    println("New function: ", new_func)
+    reform_con[end] = JuMP.build_constraint(error, new_func, _MOI.Nonpositives(d))
     return vcat(reform_con)
 end
 
+function reformulate_disjunct_constraint(
+    model::JuMP.AbstractModel,
+    con::JuMP.VectorConstraint{T, S, R},
+    bvref::Union{JuMP.AbstractVariableRef, JuMP.GenericAffExpr},
+    method::PSplit
+) where {T, S <: Union{_MOI.Nonnegatives}, R}
+    p = length(method.partition)
+    d = con.set.dimension
+    v = @variable(model, [1:p,1:d])
+    reform_con = Vector{JuMP.AbstractConstraint}(undef, p + 1)
+    constants = Vector{Number}(undef, d)
+    for i in 1:p
+        #I should be subtracting the constant here.
+        partitioned_expressions = [_build_partitioned_expression(con.func[j], method.partition[i]) for j in 1:d]
+        func = JuMP.@expression(model, [j = 1:d], partitioned_expressions[j][1] - v[i,j])
+        constants .= [-partitioned_expressions[j][2] for j in 1:d]
+        reform_con[i] = JuMP.build_constraint(error, -func, _MOI.Nonpositives(d))
+        for j in 1:d
+            _bound_auxiliary(model, v[i,j], -func[j], method)
+        end
+    end
+    new_func = JuMP.@expression(model,[j = 1:d], -sum(v[i,j] * bvref for i in 1:p) + constants[j]*bvref)
+    reform_con[end] = JuMP.build_constraint(error, new_func, _MOI.Nonpositives(d))
+    #TODO: how do i avoid the vcat?
+    return vcat(reform_con)
+end
+#TODO:
+function reformulate_disjunct_constraint(
+    model::JuMP.AbstractModel,
+    con::JuMP.VectorConstraint{T, S, R},
+    bvref::Union{JuMP.AbstractVariableRef, JuMP.GenericAffExpr},
+    method::PSplit
+) where {T, S <: Union{_MOI.Zeros}, R}
+    p = length(method.partition)
+    d = con.set.dimension
+    reform_con_np = Vector{JuMP.AbstractConstraint}(undef, p + 1)  # nonpositive (≤ 0)
+    reform_con_nn = Vector{JuMP.AbstractConstraint}(undef, p + 1)  # nonnegative (≥ 0)
+    v = @variable(model, [1:p,1:d,1:2])  # [i,j,1] for ≤, [i,j,2] for ≥
+    constants = Vector{Number}(undef, d)
+    for i in 1:p
+        partitioned_expressions = [_build_partitioned_expression(con.func[j], method.partition[i]) for j in 1:d]
+        
+        # Nonpositive part: func ≤ 0 → func - v ≤ 0
+        func = JuMP.@expression(model, [j = 1:d], partitioned_expressions[j][1] - v[i,j,1])        
+        constants .= [partitioned_expressions[j][2] for j in 1:d]
+        
+        reform_con_np[i] = JuMP.build_constraint(error, func, _MOI.Nonpositives(d))
+        reform_con_nn[i] = JuMP.build_constraint(error, -func, _MOI.Nonpositives(d))
+        
+        for j in 1:d
+            _bound_auxiliary(model, v[i,j,1], func[j], method)
+            _bound_auxiliary(model, v[i,j,2], -func[j], method)
+        end
+    end
+
+    # Final constraints: combine auxiliary variables with constants
+    new_func_np = JuMP.@expression(model,[j = 1:d], sum(v[i,j,1] * bvref for i in 1:p) + constants[j]*bvref)
+    new_func_nn = JuMP.@expression(model,[j = 1:d], -sum(v[i,j,2] * bvref for i in 1:p) + constants[j]*bvref)
+    reform_con_np[end] = JuMP.build_constraint(error, new_func_np, _MOI.Nonpositives(d))
+    reform_con_nn[end] = JuMP.build_constraint(error, new_func_nn, _MOI.Nonpositives(d))
+    return vcat(reform_con_np, reform_con_nn)
+end
 
 ################################################################################
 #                          FALLBACK WARNING DISPATCHES
@@ -288,8 +350,7 @@ end
 # Generic fallback for _build_partitioned_expression
 function _build_partitioned_expression(
     expr::Any,
-    ::Vector{JuMP.VariableRef},
-    ::Any
+    ::Vector{JuMP.VariableRef}
 )
     error("PSplit: _build_partitioned_expression not implemented for expression type $(typeof(expr)). Supported types: GenericAffExpr, GenericQuadExpr, VariableRef, Number, NonlinearExpr.")
 end
@@ -305,8 +366,7 @@ end
 # Generic fallback for _nonlinear_recursion
 function _nonlinear_recursion(
     expr::Any,
-    ::Vector{JuMP.VariableRef},
-    ::JuMP.ScalarConstraint
+    ::Vector{JuMP.VariableRef}
 )
     error("PSplit: _nonlinear_recursion not implemented for expression type $(typeof(expr)). Supported types: GenericAffExpr, GenericQuadExpr, VariableRef, Number, NonlinearExpr.")
 end
@@ -327,7 +387,7 @@ end
 # Generic fallback for reformulate_disjunct_constraint (scalar)
 function reformulate_disjunct_constraint(
     ::JuMP.AbstractModel,
-    con::JuMP.ScalarConstraint,
+    con::Any,
     ::Union{JuMP.AbstractVariableRef, JuMP.GenericAffExpr},
     ::PSplit
 )
@@ -337,11 +397,11 @@ end
 # Generic fallback for reformulate_disjunct_constraint (vector)
 function reformulate_disjunct_constraint(
     ::JuMP.AbstractModel,
-    con::JuMP.VectorConstraint,
+    con::Any,
     ::Union{JuMP.AbstractVariableRef, JuMP.GenericAffExpr},
     ::PSplit
 )
-    error("PSplit: reformulate_disjunct_constraint not implemented for vector constraint set type $(typeof(con.set)). Supported types: Zeros, Nonpositives, Nonnegatives.")
+    error("PSplit: reformulate_disjunct_constraint not implemented for vector constraint set type $(typeof(con)). Supported types: VectorConstraint of _MOI.Nonnegatives, _MOI.Nonpositives, _MOI.Zeros.")
 end
 
 # Generic fallback for _set_values
